c4bbda352fef69d51a20253f42d6b99969671851
KravAD <dkrav@myuax.com>
2023-11-29 19:22:24 +0100

    commit 24
    Función que saca el valor mas grande del vector y lo imprime.

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int main() {
-    vector<int> numeros = {50, 12, 10, 32};
+    vector<int> numeros = {50, 12, 100,32};
     sort(numeros.begin(), numeros.end());
 
 for (int num : numeros) {
     cout << num << endl;
 }
-
+int maxNum = *max_element(numeros.begin(), numeros.end());
+cout << "El numero maximo es: " << maxNum << endl;
     return 0;
 }
