6379b5591914a1c720042d0bdf4656a463e16613
KravAD <dkrav@myuax.com>
2023-11-29 18:55:06 +0100

    commit 13
    Defino list 'lista'  de tipo float y stack de la lista tipo float.

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <list>
 #include <stack>
+using namespace std;
 
 int main() {
-
+list<float> lista ={1.2, 2.3, 3.4};
+stack<float, list<float>> pila(lista);
 
     return 0;
 }
