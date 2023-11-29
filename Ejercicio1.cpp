d55db9e372128575453452e1b972b6df2fc5740c
KravAD <dkrav@myuax.com>
2023-11-29 18:39:00 +0100

    commit 7
    Bucle for. Inicialización con numeros.begin, condición de seguir bucle hasta que iterador it no haya alcanzado el final del vector numeros.end y la acción de ++i para que apunte al siguiente elemento del vector.

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main(){
 vector<int> numeros = {10, 3, 40};
+for(vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+    cout << *it << endl;
+}
 
     return 0;
 }
