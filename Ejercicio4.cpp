dbd1535dfb22441f8bc762796e72f0cf5d0cf162
KravAD <dkrav@myuax.com>
2023-11-29 19:45:27 +0100

    commit 31
    Por pantalla saca el cálculo de la suma y la media de los valores del valarray.

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() {
     valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
+    cout << "la suma es: " << valores.sum() << endl;
+    cout << "la media es: " << valores.sum() / valores.size() << endl;
+
 
     return 0;
 }
