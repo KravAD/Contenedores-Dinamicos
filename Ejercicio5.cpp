f58f0fe571cc24a090fcdfb70102c28137babced
KravAD <dkrav@myuax.com>
2023-11-29 20:22:46 +0100

    commit 37
    Creo el vector valores de tipo int con valores. Uso la función for_each() que iterará para cada valor del vector números y los imprimirá por pantalla. también incluyo algorithm para poder usar la funci´pon for_each().
    Se da valor a ptr en la función shared_ptr y luego lo imprimirá usando *ptr.

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <algorithm>
 using namespace std;
 int main() {
 auto cuadrado = [] (int num) {return num * num;};
 cout << cuadrado(5)<< endl;
+
+    vector<int> numeros = {1, 2, 3, 4, 5};
+    for_each(numeros.begin(), numeros.end(), [](int num) {
+        cout << num << endl;
+    });
+
+    shared_ptr<int> ptr(new int(10));
+    cout << "Valor apuntado por ptr: " << *ptr << endl;
     return 0;
 }
