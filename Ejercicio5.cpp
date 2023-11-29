a54d244ad230ba94547a4120a64cad51bca896f9
KravAD <dkrav@myuax.com>
2023-11-29 20:14:30 +0100

    commit 36
    Incluyo using namespace std y en la función main utilizo expresión lambda que hace el cuadrado de un numero.

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-
+using namespace std;
 int main() {
-
+auto cuadrado = [] (int num) {return num * num;};
+cout << cuadrado(5)<< endl;
     return 0;
 }
